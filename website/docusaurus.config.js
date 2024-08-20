b5b96b164c0883ed2ae0b8279c6b1103a25858fa
andrew-lastmile <129882602+andrew-lastmile@users.noreply.github.com>
2024-08-20 13:37:47 -0400

    Updates to font, colors, dark/light mode switching, alignment
    - Font scheme to match website
    - Color scheme to match website
    - Disabling dark and light mode
    - Aligning TOC to the left side
    - Reducing font size

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -103,7 +103,7 @@ const config = {
       },
       colorMode: {
         defaultMode: "dark",
-        disableSwitch: false,
+        disableSwitch: true,
         respectPrefersColorScheme: false,
       },
       prism: {
